1941203063734e1a2445dd6880e8dfe48efd86be
choux-croquant <choifrance@gmail.com>
2025-11-18 03:22:04 +0900

    update github actions deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://choux-croquant.github.io/',
-	base: '/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://choux-croquant.github.io/',
+  base: '/',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
